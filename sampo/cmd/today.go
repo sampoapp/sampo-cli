0d9cefd6853e7d08435eac2c719846746066eb1b
Arto Bendiken <arto@sampoapp.org>
2018-11-03 23:14:31 +0200

    Imported scaffold code adapted from `cobra add ...`.

---
+++ sampo/cmd/today.go
@@ -0,0 +1,38 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// todayCmd represents the today command
+var todayCmd = &cobra.Command{
+	Use:   "today",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("today called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(todayCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// todayCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// todayCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
