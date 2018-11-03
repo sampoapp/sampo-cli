b2a2e5cf5e9acd1fcbe6b9559d84fdd51aa3c849
Arto Bendiken <arto@sampoapp.org>
2018-11-03 22:46:24 +0200

    Imported scaffold code adapted from `cobra init sampo`.

---
+++ sampo/main.go
@@ -0,0 +1,10 @@
+/* This is free and unencumbered software released into the public domain. */
+
+// sampo provides a command-line interface (CLI) for Sampo.
+package main
+
+import "github.com/sampoapp/sampo-cli/sampo/cmd"
+
+func main() {
+	cmd.Execute()
+}
