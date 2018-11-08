db9292f7c7406c413d4c3257dfdaf915a0a8b985
Arto Bendiken <arto@sampoapp.org>
2018-11-08 17:59:29 +0200

    Fleshed out `sampo build`.

--- sampo/cmd/build.go
+++ sampo/cmd/build.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/sampoapp/sampo-cli/sampo/store"
 	"github.com/spf13/cobra"
@@ -20,51 +21,78 @@ This is the command-line interface (CLI) for Sampo.`,
 	Args: cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		// Read the schema file:
-		sqlSchemaData, err := ioutil.ReadFile(fmt.Sprintf("%s/.sampo/schema.sql", os.Getenv("HOME")))
-		if err != nil {
-			panic(err)
-		}
-		sqlSchema := string(sqlSchemaData)
+		sqlSchema := readSchemaFile()
 
 		// Validate all input arguments:
 		for _, arg := range args {
-			info, err := os.Stat(arg)
-			if err != nil {
-				if os.IsNotExist(err) {
-					fmt.Printf("%s does not exist\n", arg)
-					os.Exit(66) // EX_NOINPUT
-				}
-				panic(err)
-			}
-			if !info.IsDir() {
-				fmt.Printf("%s is not a directory\n", arg)
-				os.Exit(66) // EX_NOINPUT
-			}
+			validateInputDirectory(arg)
 		}
 
 		// Process the input arguments:
 		for _, arg := range args {
-			db, err := store.Create(fmt.Sprintf("%s.db", arg))
-			if err != nil {
-				panic(err)
-			}
-			defer db.Close()
-
-			if err := db.Init(sqlSchema); err != nil {
-				panic(err)
-			}
-
-			if _, err := db.CreateUser("owner", "Owner"); err != nil {
-				panic(err)
-			}
-
-			if err := db.Compact(); err != nil {
-				panic(err)
-			}
+			createSnapshot(arg, sqlSchema)
 		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(BuildCmd)
+}
+
+func readSchemaFile() string {
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s/.sampo/schema.sql", os.Getenv("HOME")))
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+func validateInputDirectory(arg string) {
+	info, err := os.Stat(arg)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("%s does not exist\n", arg)
+			os.Exit(66) // EX_NOINPUT
+		}
+		panic(err)
+	}
+	if !info.IsDir() {
+		fmt.Printf("%s is not a directory\n", arg)
+		os.Exit(66) // EX_NOINPUT
+	}
+}
+
+func createSnapshot(inputDirPath string, sqlSchema string) {
+	db, err := store.Create(fmt.Sprintf("%s.db", inputDirPath))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if err := db.Init(sqlSchema); err != nil {
+		panic(err)
+	}
+
+	if _, err := db.CreateUser("owner", "Owner"); err != nil {
+		panic(err)
+	}
+
+	var yamlFiles []string
+	err = filepath.Walk(inputDirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil && !info.IsDir() && filepath.Ext(path) == ".yaml" {
+			yamlFiles = append(yamlFiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, yamlFile := range yamlFiles {
+		fmt.Println(yamlFile) // TODO: process the YAML file
+	}
+
+	if err := db.Compact(); err != nil {
+		panic(err)
+	}
 }
